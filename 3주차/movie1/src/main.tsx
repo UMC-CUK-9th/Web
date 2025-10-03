2145ae22fddd4e6f8a51d07d5e1fed00086cd4e0
Dobby19 <southvi1@naver.com>
2025-10-03 18:26:12 +0900

    File 구조 설정

--- 3주차/movie1/src/main.tsx
+++ 3주차/movie1/src/main.tsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { TodoProvider } from './context/TodoProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TodoProvider>
-      <App />
-    </TodoProvider>
+    <App />
   </StrictMode>,
 )
