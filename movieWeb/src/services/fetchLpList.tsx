c294d4556f1c03a58c6edb8f53a99aa19363f7b1
oyejin0925 <142577684+oyejin0925@users.noreply.github.com>
2025-11-07 16:58:24 +0900

    Update fetchLpList.tsx

--- movieWeb/src/services/fetchLpList.tsx
+++ movieWeb/src/services/fetchLpList.tsx
@@ -5,7 +5,7 @@ const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
 
 export const fetchLpList = async (sort: "asc" | "desc"): Promise<LpItem[]> => {
   // await new Promise((r) => setTimeout(r, 1500)); //로딩확인용 
-  const { data } = await axios.get(`${BASE_URL}/v1/lp4s`, {
+  const { data } = await axios.get(`${BASE_URL}/v1/lps`, {
     params: {
       cursor: 0,
       limit: 10,
